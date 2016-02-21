281c67928ddfa80340f124298c0db0809ab01e93
tsvetelintsonev <github.tsonev@yahoo.co.uk>
2016-02-21 12:46:33 +0100

    main added

---
+++ Harbor-containers-handling-school-assignment2/src/harbor/Main.java
@@ -0,0 +1,128 @@
+package harbor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Container> containers = new ArrayList<>();
+		int i = 0;
+		
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 1), LocalDate.of(2016, 3, 5)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 2), LocalDate.of(2016, 3, 5)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 3), LocalDate.of(2016, 3, 5)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 5)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 4), LocalDate.of(2016, 3, 5)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 6)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 6)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 5), LocalDate.of(2016, 3, 6)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 7)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 7)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 6), LocalDate.of(2016, 3, 7)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 8)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 8)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 7), LocalDate.of(2016, 3, 8)));
+		
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 9)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 9)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 8), LocalDate.of(2016, 3, 9)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 9), LocalDate.of(2016, 3, 10)));
+
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 11)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 25)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 11)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 11)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 10)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 11)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 15)));
+		containers.add(new Container(i++, LocalDate.of(2016, 3, 10), LocalDate.of(2016, 3, 11)));
+
+		System.out.println(containers);
+		
+
+	}
+
+}
